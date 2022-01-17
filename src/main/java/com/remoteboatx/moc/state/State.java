0dac60815094b50da4a436ba7ee238053ea230ce
Yannick Zapfe <yannickzapfe@posteo.de>
2022-01-17 15:25:22 +0200

    Display connected vessels in frontend.

--- src/main/java/com/remoteboatx/moc/state/State.java
+++ src/main/java/com/remoteboatx/moc/state/State.java
@@ -36,17 +36,27 @@ public class State {
 
     public void addVessel(String vesselId) {
         vessels.put(vesselId, new Vessel());
+
+        // TODO: Format messages to frontend properly.
+        frontendMessageHandler.sendMessage(String.format("{\"%s\": {\"connected\": true}}",
+                vesselId));
     }
 
     public void removeVessel(String vesselId) {
         vessels.remove(vesselId);
+
+        // TODO: Format messages to frontend properly.
+        frontendMessageHandler.sendMessage(String.format("{\"%s\": {\"connected\": false}}",
+                vesselId));
     }
 
     public void updateLatency(String vesselId, Latency latency) {
         vessels.get(vesselId).setLatency(latency);
 
         // TODO: Format messages to frontend properly using latency class.
-        frontendMessageHandler.sendMessage(String.format("outgoing: %d, incoming: %d, round " +
-                "trip: %d", latency.getOutgoing(), latency.getIncoming(), latency.getRoundTrip()));
+        String latencyMessage = String.format("outgoing: %d, incoming: %d, round trip: %d",
+                latency.getOutgoing(), latency.getIncoming(), latency.getRoundTrip());
+        frontendMessageHandler.sendMessage(String.format("{\"%s\": {\"latency\": \"%s\"}}",
+                vesselId, latencyMessage));
     }
 }
--- src/main/resources/static/frontend/frontend.js
+++ src/main/resources/static/frontend/frontend.js
@@ -3,11 +3,26 @@ let ws;
 function connect() {
     ws = new WebSocket("ws://localhost:8080/frontend");
     ws.onmessage = function (e) {
-        printMessage(e.data);
+        handleMessage(e.data);
     }
     document.getElementById("connectButton").disabled = true;
     document.getElementById("connectButton").value = "Connected";
     document.getElementById("name").disabled = true;
+}
+
+function handleMessage(data) {
+    const messageData = JSON.parse(data);
+    for (vesselId in messageData) {
+        const message = messageData[vesselId];
+        if (message.connected === true) {
+            addVessel(vesselId);
+        } else if (message.connected === false) {
+            removeVessel(vesselId);
+        }
+        if (message.latency) {
+            printMessage(vesselId + ": " + message.latency);
+        }
+    }
 }
 
 function printMessage(data) {
@@ -17,6 +32,20 @@ function printMessage(data) {
     messages.appendChild(newMessage);
 }
 
+function addVessel(vesselId) {
+    let vessels = document.getElementById("vessels");
+    let newVessel = document.createElement("div");
+    newVessel.id = vesselId;
+    newVessel.innerHTML = vesselId;
+    vessels.appendChild(newVessel);
+}
+
+function removeVessel(vesselId) {
+    let vessel = document.getElementById(vesselId);
+    vessel.parentNode.removeChild(vessel);
+}
+
+// TODO: Use for disconnect.
 function sendToGroupChat() {
     let messageText = document.getElementById("message").value;
     document.getElementById("message").value="";
