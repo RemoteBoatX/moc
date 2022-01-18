8ae53fc9611f0722424c611232413a5086e35775
Yannick Zapfe <yannickzapfe@posteo.de>
2022-01-18 15:30:02 +0200

    Setup JUnit with example tests.

---
+++ src/test/java/com/remoteboatx/moc/message/LatencyMessageTest.java
@@ -0,0 +1,16 @@
+package com.remoteboatx.moc.message;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LatencyMessageTest {
+
+    @Test
+    void testSuccess() {
+    }
+
+    @Test
+    void testFail() {
+        Assertions.fail("test failed");
+    }
+}
