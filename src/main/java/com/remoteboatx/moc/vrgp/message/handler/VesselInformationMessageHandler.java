b8240671bc63bc21e000965ef6d5262403377a44
Yannick Zapfe <yannickzapfe@posteo.de>
2022-02-10 16:54:47 +0200

    Fix merge conflicts.

--- src/main/java/com/remoteboatx/moc/vrgp/message/handler/VesselInformationMessageHandler.java
+++ src/main/java/com/remoteboatx/moc/vrgp/message/handler/VesselInformationMessageHandler.java
@@ -2,7 +2,6 @@ package com.remoteboatx.moc.vrgp.message.handler;
 
 import com.remoteboatx.moc.state.State;
 import com.remoteboatx.moc.vrgp.message.VesselInformation;
-import com.remoteboatx.moc.vrgp.message.VesselInformationMessage;
 import com.remoteboatx.moc.vrgp.message.VrgpMessage;
 import com.remoteboatx.moc.websocket.WebSocketAction;
 
@@ -20,7 +19,7 @@ public class VesselInformationMessageHandler implements VrgpSingleMessageHandler
     }
 
     @Override
-    public Function<VrgpMessage, VesselInformationMessage> getSingleMessage() {
+    public Function<VrgpMessage, VesselInformation> getSingleMessage() {
         return VrgpMessage::getVessel;
     }
 }
